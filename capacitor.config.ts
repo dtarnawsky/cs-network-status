9dc73557861199254e3ce4136f1401107a73b253
Damian Tarnawsky <damian@ionic.io>
2023-03-23 08:09:32 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.network.status',
+  appName: 'cs-network-status',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
