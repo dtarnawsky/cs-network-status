657a0eb9da25bb682f7c6a786246d10c6baea8ee
dtarnawsky <damian@ionic.io>
2023-05-23 09:38:51 -0700

    feat: get status on resume

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 import { Network } from '@capacitor/network';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +12,27 @@ export class HomePage implements OnInit {
   status: string = 'Unknown';
   constructor(private ngZone: NgZone) { }
 
-  ngOnInit(): void {
-    Network.addListener('networkStatusChange', status => {
+  async ngOnInit() {
+    const status = await Network.getStatus();
+    this.status = status.connectionType;
+    await Network.addListener('networkStatusChange', status => {
       console.log('Network status changed', JSON.stringify(status));
       this.ngZone.run(() => {
         this.status = status.connected ? status.connectionType : 'offline';
       });
 
     });
+    App.addListener('resume', async () => {
+      const status = await Network.getStatus();
+      this.ngZone.run(() => {
+        this.status = status.connectionType;
+      });
+    });
+  }
+
+  async getStatus() {
+    const status = await Network.getStatus();
+    alert(JSON.stringify(status));
   }
 
 }
